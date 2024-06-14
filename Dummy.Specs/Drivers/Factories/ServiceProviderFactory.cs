f7c4c9d4720a22dd29a44cce8dc576e08ce0b784
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-14 20:12:58 -0300

    created helper classes for tests enhancement

---
+++ Dummy.Specs/Drivers/Factories/ServiceProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Dummy.Specs.Drivers.Factories;
+
+public class ServiceProviderFactory
+{
+    public static IServiceProvider CreateServiceProvider()
+    {
+        var services = new ServiceCollection();
+        var configuration = ConfigurationFactory.CreateConfiguration();
+
+        services.AddSingleton(configuration);
+
+        return services.BuildServiceProvider();
+    }
+}
